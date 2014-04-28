da7c1c654acb3d6c54c930d19c9e45989b3c19ae
Hack Reactor Students <michael.p.freeman@gmail.com>
2014-04-28 15:57:52 -0700

    Implemented send and ordered fetch requests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,30 +1,60 @@
 // YOUR CODE HERE:
 
 var app = {};
+app.lastUpdate = Date.now();
+
 app.init = function () {
+  $('#sendButton').click(function(event) {
+    var username = window.location.search.match(/username=([\w]*)/);
+    username = username[1] || 'Anonymous';
+    var message = $('#inputMsg').val();
+    app.send({
+      username: username,
+      text: message,
+      roomname: 'Lobby'
+    });
+  });
   app.fetch();
 };
 
-app.send = function() {
+app.send = function(msgObject) {
+  msgObject.username = msgObject.username || 'Anonymous';
+  msgObject.text = msgObject.text || '';
+  msgObject.roomname = msgObject.roomname || 'Lobby';
+
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
-
+    type: 'POST',
+    data: JSON.stringify(msgObject),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      app.fetch();
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
   });
 };
 
 app.fetch = function() {
   $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+    where: {
+      'createdAt': {'$gte': app.lastUpdate}
+    },
     type: 'GET',
     success: function (data) {
+      console.log(data);
       _.each(data.results, function(msgObject) {
         msgObject.username = msgObject.username || 'Anonymous';
         msgObject.text = msgObject.text || '';
         app.render(msgObject);
       });
+      app.lastUpdate = data.results[99].createdAt;
     },
     error: function (data) {
-      console.error('chatterbox: Failed to send message');
+      console.error('chatterbox: Failed to fetch messages');
     }
   });
 };
@@ -34,4 +64,6 @@ app.render = function (msgObject) {
   $('#main').append(template(msgObject));
 };
 
-app.init();
+$(document).ready(function() {
+  app.init();
+});
